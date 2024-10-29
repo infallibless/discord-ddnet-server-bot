899dcc9d1ef0707566869fee9cccf5d2778517cb
raynex <dwaadwwd7@gmail.com>
2024-10-29 17:24:27 +0300

    discord-ddnet-server-bot
    a discord bot that shows ddnet server statistics

---
+++ ddnet.js
@@ -0,0 +1,31 @@
+const { Client } = require('discord.js');
+const axios = require('axios');
+const client = new Client({ intents: 513 });
+const botoken = "HERE"
+const ip = 'SERVER_IP';
+const port = 8341; // port
+const api = `http://${ip}:${port}/api/`;
+
+client.once('ready', () => {
+    console.log(`${client.user.tag} online`);
+    up();
+    setInterval(up, 10 * 60 * 1000); 
+});
+
+async function up() {
+    try {
+        const response = await axios.get(`${api}status`);
+        const data = response.data;
+        const playerCount = data?.players?.count;
+        client.user.setActivity(`${playerCount} players active`, { type: 'PLAYING' });
+        console.log(`playing status updated -> 14 players active`);
+    } catch (error) {
+        console.error('failed to retrieve server status:', error.message);
+        client.user.setActivity(`unknown server`, { type: 'PLAYING' });
+    }
+}
+
+
+
+
+client.login(botoken);
